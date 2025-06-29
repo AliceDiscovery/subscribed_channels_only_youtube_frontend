c396d755595cc192212daa5ffe2d82b1c9a16195
AliceDiscovery <whittockalice+github@gmail.com>
2025-06-29 16:04:19 +0100

    required login before calling pages that run API functions

--- app/routes/_page_routes.py
+++ app/routes/_page_routes.py
@@ -1,5 +1,6 @@
 """ define all webpage routes """
 from flask import Blueprint, render_template
+from flask_login import login_required
 from app.youtube_api import YouTubeAPI
 
 
@@ -18,6 +19,7 @@ def search():
 
 
 @page_bp.route('/search/<search_terms>')
+@login_required
 def search_results(search_terms):
     search_results_first_page = youtube.get_requests.fetch_search_results(search_terms)
     return render_template(
@@ -27,6 +29,7 @@ def search_results(search_terms):
 
 
 @page_bp.route('/channel/<channel_id>')
+@login_required
 def channel_overview(channel_id):
     channel_info = youtube.fetch_channel_info(channel_id)
     channel_videos_first_page = youtube.get_requests.fetch_channel_videos(channel_id)
@@ -38,6 +41,7 @@ def channel_overview(channel_id):
 
 
 @page_bp.route('/video/<video_id>')
+@login_required
 def video_page(video_id):
     video_data = youtube.fetch_video_info(video_id)
     video_comments_first_page = youtube.get_requests.fetch_video_comments(video_id)
