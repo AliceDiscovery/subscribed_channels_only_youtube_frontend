658e0e3be5f6cbf790094426300ff8435eebf466
AliceDiscovery <whittockalice+github@gmail.com>
2025-06-30 08:40:09 +0100

    created page route for /logout

--- app/routes/_accounts_routes.py
+++ app/routes/_accounts_routes.py
@@ -1,6 +1,6 @@
 """ define all webpage routes relating to user accounts """
-from flask import Blueprint, request, render_template, jsonify
-from flask_login import login_user, login_required, current_user
+from flask import Blueprint, request, render_template, redirect, url_for, jsonify
+from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from ..database import db, User
@@ -47,6 +47,13 @@ def login():
         return jsonify({"error": "Invalid email or password"}), 401
 
     return render_template('login.html')
+
+
+@accounts_bp.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('accounts.login'))
 
 
 @accounts_bp.route('/profile')
