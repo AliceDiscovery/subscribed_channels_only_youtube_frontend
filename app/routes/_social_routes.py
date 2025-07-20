236151044b06351818a115f8a1c9e3637a4dbd96
AliceDiscovery <whittockalice+github@gmail.com>
2025-07-20 18:02:35 +0200

    Added button for creating a demo content pack.

--- app/routes/_social_routes.py
+++ app/routes/_social_routes.py
@@ -1,14 +1,40 @@
 """ define all page routes relating to social aspects of the website """
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
+
+from ..database import db, User, ContentPack, UserContentPack
 
 
 social_bp = Blueprint('social', __name__)
 
 
-@social_bp.route('/content-filters')
+@social_bp.route('/content-filters', methods=['GET', 'POST'])
 @login_required
 def user_content_packs():
+    def create_content_pack(user_id: int, pack_name: str, is_public: bool = False, is_enabled: bool = True):
+        """ create a content pack and set it's state """
+        user = User.query.get(user_id)
+        if not user:
+            raise ValueError(f'User with ID {user_id} does not exist.')
+
+        new_pack = ContentPack(
+            name=pack_name,
+            author_id=user_id,
+            is_public=is_public
+        )
+        db.session.add(new_pack)
+        db.session.flush()
+
+        link = UserContentPack(
+            user_id=user_id,
+            content_pack_id=new_pack.id,
+            is_enabled=is_enabled
+        )
+        db.session.add(link)
+        db.session.commit()
+
+    if request.method == 'POST':
+        create_content_pack(current_user.id, pack_name='demo pack')
     content_packs = [
         {
             'content_pack_id': link.content_pack.id,
