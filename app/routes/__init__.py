ebc84bb047ef25b920974afc5bafa083d370ec13
AliceDiscovery <whittockalice+github@gmail.com>
2025-07-20 16:55:35 +0200

    Created routing file for the social aspects of the website.

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,5 +1,6 @@
 from ._page_routes import page_bp
 from ._data_routes import data_bp
 from ._accounts_routes import accounts_bp
+from ._social_routes import social_bp
 
-BLUEPRINTS = [page_bp, data_bp, accounts_bp]
+BLUEPRINTS = [page_bp, data_bp, accounts_bp, social_bp]
---
+++ app/routes/_social_routes.py
@@ -0,0 +1,5 @@
+""" define all page routes relating to social aspects of the website """
+from flask import Blueprint
+
+
+social_bp = Blueprint('social', __name__)
