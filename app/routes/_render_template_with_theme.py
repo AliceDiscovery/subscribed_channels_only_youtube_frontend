0811cd5b3732bb52474cb0db6b6736b2dcf4edfe
AliceDiscovery <whittockalice+github@gmail.com>
2025-09-02 21:28:21 +0200

    Confirm that the current use is authenticated before trying to load user themes.

--- app/routes/_render_template_with_theme.py
+++ app/routes/_render_template_with_theme.py
@@ -7,7 +7,7 @@ from ..load_default_themes import get_themes as get_default_themes
 def render_template_with_theme(*args, **kwargs):
     default_themes_array = get_default_themes()
 
-    if current_user.active_default_theme is not None:
+    if current_user.is_authenticated and current_user.active_default_theme is not None:
         active_user_theme_id = current_user.active_default_theme
         for theme in default_themes_array:
             if theme.get('id') == active_user_theme_id:
